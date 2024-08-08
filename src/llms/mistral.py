dfeb11097e5e34655c0cbedab5048aba316299f8
Noor Maulida <noormaulida.dev@gmail.com>
2024-08-08 20:20:15 +0700

    #15 Refactor and API adjustment

--- src/llms/mistral.py
+++ src/llms/mistral.py
@@ -1,24 +1,23 @@
 from mistralai import Mistral
 
-SYSTEM_INSTRUCTIONS = "You're a helpful assistant."
-model = "mistral-large-latest"
-api_key = "ULUoH7kuftwlI2XfMgXajhh6CwkjZ2JX"
+
+class MistralClient:    
+    def __init__(self):
+        self.model_type = "mistral-large-latest"
+        self.api_key = "ULUoH7kuftwlI2XfMgXajhh6CwkjZ2JX"
+        self.model = Mistral(api_key=self.api_key)
 
 
-def init_mistral():
-    return Mistral(api_key=api_key)
-
-
-def send_request_mistral(client, message):
-    messages = [
-        {
-            "role": "user",
-            "content": message,
-        },
-    ]
-    
-    chat_response = client.chat.complete(
-       model=model,
-       messages=messages,
-    )
-    return chat_response.choices[0].message.content
+    def send_request(self, message):
+        messages = [
+            {
+                "role": "user",
+                "content": message,
+            },
+        ]
+        
+        chat_response = self.model.chat.complete(
+        model=self.model_type,
+        messages=messages,
+        )
+        return chat_response.choices[0].message.content
